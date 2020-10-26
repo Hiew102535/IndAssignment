b5a49e1c4e25d3f1e52ad857238e97ec17726d78
Hiew102535 <70246766+Hiew102535@users.noreply.github.com>
2020-10-26 13:51:24 +0800

    Update try.js

--- try.js
+++ try.js
@@ -1,4 +1,16 @@
+function sendCNYwhatsapp(){
+	inputName = document.getElementById("name").value;
+    window.location = 'https://wa.me/?text='+inputName+'Wishing you a Happy Chinese New Year 2020 %0A%0AOpen this https://hiew102535.github.io/IndAssignment/CNY.html?name=';
+}
+function sendSHRwhatsapp(){
+	inputName = document.getElementById("name").value;
+    window.location = 'https://wa.me/?text='+inputName+'Wishing you a Selamat Hari Raya 2020 %0A%0AOpen this https://hiew102535.github.io/IndAssignment/SHY.html?name=';
+}
+function sendHDwhatsapp(){
+	inputName = document.getElementById("name").value;
+    window.location = 'https://wa.me/?text='+inputName+'Wishing you a Happy Deepavali 2020 %0A%0AOpen this https://hiew102535.github.io/IndAssignment/HD.html?name=';
+}
 function sendMCwhatsapp(){
-	inputVal = document.getElementById("name").value;
-    window.location = 'https://wa.me/?text='+inputVal+' Wishing you a Merry Christmas 2020 %0A%0AOpen this https://hiew102535.github.io/IndAssignment/MC.html?name='+outputVal;
+	inputName = document.getElementById("name").value;
+    window.location = 'https://wa.me/?text='+inputName+'Wishing you a Merry Christmas 2020 %0A%0AOpen this https://hiew102535.github.io/IndAssignment/MC.html?name=';
 }
