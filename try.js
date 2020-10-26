8387fcf1b57ad704bd79e8691879928f94f81ea2
Hiew102535 <70246766+Hiew102535@users.noreply.github.com>
2020-10-26 12:38:30 +0800

    Add files via upload

---
+++ try.js
@@ -0,0 +1,4 @@
+function sendMCwhatsapp(){
+	inputVal = document.getElementById("name").value;
+    window.location = 'https://wa.me/?text='+inputVal+' Wishing you a Merry Christmas 2020 %0A%0AOpen this https://hiew102535.github.io/IndAssignment/MC.html?name='+outputVal;
+}
